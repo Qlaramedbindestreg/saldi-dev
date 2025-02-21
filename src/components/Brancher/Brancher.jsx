f4ad6a799b97c041599dda216f19ae3798d49764
Qlaramedbindestreg <u2470787@rts-365.dk>
2025-02-21 14:12:30 +0100

    feat: implement expandable Brancher component with initial and additional branches; enhance styling for better layout

--- src/components/Brancher/Brancher.jsx
+++ src/components/Brancher/Brancher.jsx
@@ -1,41 +1,65 @@
 import './Brancher.scss';
 import { BiSolidShoppingBags } from "react-icons/bi";
-import { FaLink, FaShoppingCart, FaWarehouse } from "react-icons/fa";
-
-
+import { FaLink, 
+    FaShoppingCart, 
+    FaWarehouse } 
+from "react-icons/fa";
+import React, { useState } from 'react';
 
 
 export default function Brancher() {
 
-    return (
-    <>
-          <div className='titel' >
-            <div className='line'></div>
-        <h1>Vi leverer blandt andet til</h1>
-        </div>
-  
-    <div className='branche-wrappper'>
-     <div className="branche-container"> 
-     <FaWarehouse size={30}/>
-     <br />
-         Grossister
-         </div>
-     <div className="branche-container"> 
-     <BiSolidShoppingBags size={30}/>
-     <br />
-        Fysiske butikker
-        </div>
-     <div className="branche-container"> 
-        <FaLink size={30}/>
-        <br />
-        B2C & B2B
-        </div>
-     <div className="branche-container"> 
-       <FaShoppingCart size={30}/>
-       <br />
-        Webshops 
-        </div>
-    </div>
-    </>
-    )
+        const [showMore, setShowMore] = useState(false);
+      
+        const initialBranches = [
+          { icon: <FaWarehouse size={30} />, label: 'Grossister' },
+          { icon: <BiSolidShoppingBags size={30} />, label: 'Fysiske butikker' },
+          { icon: <FaLink size={30} />, label: 'B2C & B2B' },
+          { icon: <FaShoppingCart size={30} />, label: 'Webshops' },
+        ];
+      
+        const additionalBranches = [
+          { icon: <FaWarehouse size={30} />, label: 'Branche' },
+          { icon: <BiSolidShoppingBags size={30} />, label: 'Branche' },
+          { icon: <FaLink size={30} />, label: 'Branche' },
+          { icon: <FaShoppingCart size={30} />, label: 'Branche' },
+        ];
+      
+        return (
+          <>
+            <div className='titel'>
+              <div className='line'></div>
+              <h1>Vi leverer blandt andet til</h1>
+            </div>
+      
+            <div className='branche-wrapper'> 
+              {initialBranches.map((branch, index) => (
+                <div className='branche-container' key={index}>
+                  {branch.icon}
+                  <br />
+                  {branch.label}
+                </div>
+              ))}
+      
+              {showMore &&
+                additionalBranches.map((branch, index) => (
+                  <div className='branche-container' key={index + initialBranches.length}>
+                    {branch.icon}
+                    <br />
+                    {branch.label}
+                  </div>
+                ))}
+            </div>
+      
+            <div>
+              <button
+                onClick={() => setShowMore(!showMore)}
+                className='branche-button'
+              >
+                {showMore ? 'Vis færre' : 'Vis flere'}
+              </button>
+            </div>
+          </>
+        );
 }
+    
