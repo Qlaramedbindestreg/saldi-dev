4083be5e16f0e4390f349b5862c5249feec5414d
Qlaramedbindestreg <u2470787@rts-365.dk>
2025-03-03 10:43:15 +0100

    feat: implement newTab component for improved login options; refactor LogIn component to use anchor link for navigation

--- src/components/LogIn/LogIn.jsx
+++ src/components/LogIn/LogIn.jsx
@@ -1,35 +1,15 @@
 import { CiLogin } from "react-icons/ci";
-import { useNavigate } from "react-router-dom";
 import './LogIn.scss';
 
 export default function LogIn() {
-  const navigate = useNavigate();
-
-  const fetchData = async () => {
-    try {
-      const response = await fetch("https://undefined-link.com/api/login"); 
-      if (!response.ok) {
-        throw new Error(`HTTP error! Status: ${response.status}`);
-      }
-      const result = await response.json();
-
-
-      localStorage.setItem("fetchedData", JSON.stringify(result));
-
- 
-      navigate("/newTab", { target: "_blank" }); 
-    } catch (err) {
-      console.error("Error fetching data: ", err.message);
-    }
-  };
-
   return (
     <>
       <div className="login-container">
         <CiLogin />
-        <p onClick={fetchData} style={{ cursor: "pointer" }}>
+        {/* Open the newTab component in a new tab */}
+        <a href="/newTab" target="_blank" style={{ cursor: "pointer" }}>
           Log Ind
-        </p>
+        </a>
       </div>
     </>
   );
